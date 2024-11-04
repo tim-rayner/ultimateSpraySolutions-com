cdaff1685ad598f82d838f11919106b6e93f3a21
Tim Rayner <tim.rayner@liftshare.com>
2024-11-04 13:55:42 +0000

    init: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold my-3"> Ultimate Spray Solutions</h1>
+      <h4 className="text-xl font-semibold my-3"> Automotive</h4>
+      <p className="italic my-3"> 👷 Site Under Construction 👷</p>
+    </div>
+  );
+}
+
+export default App;
